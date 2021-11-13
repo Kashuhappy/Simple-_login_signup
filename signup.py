a32cb14e5d4fca7400d8ab6d08b46316dc7bed47
Kashuhappy <knashipa@gmail.com>
2021-11-13 15:20:17 +0300

    Changes made

--- signup.py
+++ signup.py
@@ -1,4 +1,12 @@
-print("Create an account")
+print("Create an account.")
 username = input("Enter your username: ")
 password = input("Enter your passcode: ")
 
+print("You have created your acoount.\nNow log in to your account.")
+val1 = input("Username: ")
+val2 = input("Password: ")
+
+if val1 == username and val2 == password:
+    print("Welcome to your account")
+else:
+    print("Invalid credentials")
